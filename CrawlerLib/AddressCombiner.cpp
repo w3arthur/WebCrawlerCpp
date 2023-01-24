b94abee6cf046bcbcd825765dd1192e981170dbb
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2023-01-24 08:33:26 +0200

    testing

--- CrawlerLib/AddressCombiner.cpp
+++ CrawlerLib/AddressCombiner.cpp
@@ -8,10 +8,15 @@
 // check for another cases with include
 // fix //
 // test all the cases
+//move to string_view
+// default http
+// default not require www
+
 
 string combiner(string uri1, string uri2)
 {
     {    //uri 1 as copy
+        //if (uri1.find("://" != uri1.npos())
         if (uri2.find("://") != std::string::npos) return uri2;
         //else  return uri1 + uri2;
 
@@ -32,14 +37,94 @@ string combiner(string uri1, string uri2)
                    uri2 = uri2.substr(2);
                }*/
 
-        while (!uri2.empty() && uri2.substr(0, 2) == "./") { uri2 = uri2.substr(2); }
-        while (!uri2.empty() && uri2.substr(0, 3) == "../")
+
+
+     
+        
+
+        while (!uri2.empty())   //not enooght for a break condition!
+        {
+            if (uri2.size() > 2)
+            {
+                string sub1Char = uri2.substr(0, 1);  //not needed
+                string sub2Char = uri2.substr(0, 2);
+                string sub3Char = uri2.substr(0, 3);
+
+                if (sub2Char == "./") { uri2 = uri2.substr(2); }
+                else if (sub3Char == "../")
+                {
+                    uri2 = uri2.substr(3);
+                    size_t slash = uri1.find_last_of("/");
+                    size_t doubleslash = uri1.find("//") + 1;   //or :// +2
+                    string before = uri1.substr(0, slash);
+                    string after = uri1.substr(slash + 1);
+                    //bool isAnAddress
+                    if (!before.empty() && before != after && doubleslash != slash)  uri1 = before; //uri1 = uri1.substr(0, slash)
+                }
+                else if (sub2Char == "//") 
+                {
+                    uri2 = uri2.substr(2);
+                    size_t doubleslash = uri1.find("//") + 2;
+                    string before = uri1.substr(0, doubleslash);
+                    if (!before.empty()) uri1 = before;
+                    else uri1 = "";
+                    break;
+                }
+                else if (sub1Char == "/")
+                {
+                   
+                    uri2 = uri2.substr(1);
+                    size_t doubleslash = uri1.find("//") + 1;
+
+                    size_t slash = uri1.find_last_of("/");
+                    string before = uri1.substr(0, slash);
+                    string after = uri1.substr(slash + 1);;
+     
+                    while (!before.empty() && before != after && doubleslash != slash)
+                    {
+                        uri1 = before;
+                        slash = uri1.find_last_of("/");
+                        before = uri1.substr(0, slash);
+                        after = uri1.substr(slash + 1);
+                    }
+
+                    break;
+                }
+                // stupied cases when starts with ////////////////// for uri1 and 2
+                //else if (sub1Char == " ") { uri2 = "%20" + uri2.substr(1); }  //future fixes for URI address
+
+
+                else break; //no other cases awailable
+
+            }
+            else if (uri2.size() > 1 && uri2.substr(0, 2) == "..")
+            {
+                uri2 = uri2.substr(2);
+            }
+            else if (uri2.size() > 0 && uri2.substr(0, 1) == ".")
+            {
+                uri2 = uri2.substr(1);
+            }
+            else if (uri2.size() > 0 && uri2.substr(0, 1) == "/")
+            {
+                // --, copy the case ...
+            }
+            else break; //no other cases awailable
+        }
+
+
+
+
+        //if first char is ' '
+
+        while (!uri2.empty() && uri2.substr(0, 2) == "//")
         {
             //string/it subStringDistance = uri1 //find distance from the end
             //if(! (distance uri1.begin(), subStringDistance == 0 or -1 == '/') )
                 // { erase uri1.begin() - subStringDistance, uri1.end() }
         }//contains  [A-Z]/ regex
 
+        //combined rule, adding a '/' betwin uri1/uri2
         if (!uri1.empty() && !uri2.empty() && uri1.back() != '/' && uri2.front() != '/') uri1 += '/';
         string combined_uri = uri1 + uri2;
         return combined_uri;
--- CrawlerTesting/AddressCombinerTest.cpp
+++ CrawlerTesting/AddressCombinerTest.cpp
@@ -18,25 +18,7 @@ TEST(AddressCombinerTest, EnteredSecondaryFullPathAddress_ExpectedReturnTheSecon
 TEST(AddressCombinerTest, EnteredPrimaryWithSlashSecondaryWithSlash_ExpectedCombineRighBothAddress) {
 	std::string primaryAddress = "http://www.site1.com:1234/folder1/";
 	std::string secondaryAddress = "/folder2/";
-	const std::string& result = "http://www.site1.com:1234/folder1/folder2";
-	const std::string& assume = combiner(primaryAddress, secondaryAddress);
-	EXPECT_EQ(assume, result);
-}
-
-////to fix !!!
-//TEST(AddressCombinerTest, EnteredPrimaryNoSlashSecondaryWithSlash_ExpectedCombineRighBothAddress) {
-//	std::string primaryAddress = "http://www.site1.com:1234/folder1";
-//	std::string secondaryAddress = "/folder2/";			//WRONG !!!
-//	const std::string& result = "http://www.site1.com:1234/folder2";
-//	const std::string& assume = combiner(primaryAddress, secondaryAddress);
-//	EXPECT_EQ(assume, result);
-//}
-//
-//to fix !!!
-TEST(AddressCombinerTest, EnteredPrimaryNoSlashSecondaryWithDoubleSlash_ExpectedCombineRighBothAddress) {
-	std::string primaryAddress = "http://www.site1.com:1234/folder1";
-	std::string secondaryAddress = "//site2.com/folder2/";			//WRONG !!!
-	const std::string& result = "http://site2.com/folder2";
+	const std::string& result = "http://www.site1.com:1234/folder2";
 	const std::string& assume = combiner(primaryAddress, secondaryAddress);
 	EXPECT_EQ(assume, result);
 }
@@ -73,26 +55,52 @@ TEST(AddressCombinerTest, EnteredPrimaryWithSlashSecondaryWithDotSlash_ExpectedC
 	const std::string& assume = combiner(primaryAddress, secondaryAddress);
 	EXPECT_EQ(assume, result);
 }
-//
-////to fix !!!
-//TEST(AddressCombinerTest, EnteredPrimaryWithSlashSecondaryWithDoubleDotSlash_ExpectedCombineRighBothAddressWithFolderBack) {
-//	std::string primaryAddress = "http://www.site1.com:1234/folder1/";
-//	std::string secondaryAddress = "../folder2/";
-//	const std::string& result = "http://www.site1.com:1234/folder2";
-//	const std::string& assume = combiner(primaryAddress, secondaryAddress);
-//	EXPECT_EQ(assume, result);
-//}
-//
-////to fix !!!
-//TEST(AddressCombinerTest, EnteredPrimaryWithSlashSecondaryWithDoubleDotSlash_ExpectedCombineRighBothAddressNoFolderBack) {
-//	std::string primaryAddress = "http://www.site1.com:1234/";
-//	std::string secondaryAddress = "../folder2/";
-//	const std::string& result = "http://www.site1.com:1234/folder2";
-//	const std::string& assume = combiner(primaryAddress, secondaryAddress);
-//	EXPECT_EQ(assume, result);
-//}
 
 
+
+//disabling tes https://stackoverflow.com/questions/7208070/googletest-how-to-skip-a-test
+
+
+TEST(AddressCombinerTest, AddressCombiner_TrimTestForBothAddresses_ExpectedCombineRighBothAddress) {
+	std::string primaryAddress = "        http://www.site1.com:1234/folder1//////////////               ";
+	std::string secondaryAddress = "                 folder2////////////         ";
+	const std::string& result = "http://www.site1.com:1234/folder1/folder2";
+	const std::string& assume = combiner(primaryAddress, secondaryAddress);
+	EXPECT_EQ(assume, result);
+}
+
+TEST(AddressCombinerTest, EnteredPrimaryWithSlashSecondaryWithDoubleDotSlash_ExpectedCombineRighBothAddressWithFolderBack) {
+	std::string primaryAddress = "http://www.site1.com:1234/folder1/";
+	std::string secondaryAddress = "../folder2/";
+	const std::string& result = "http://www.site1.com:1234/folder2";
+	const std::string& assume = combiner(primaryAddress, secondaryAddress);
+	EXPECT_EQ(assume, result);
+}
+
+
+TEST(AddressCombinerTest, EnteredPrimaryWithSlashSecondaryWithDoubleDotSlash_ExpectedCombineRighBothAddressNoFolderBack) {
+	std::string primaryAddress = "http://www.site1.com:1234/";
+	std::string secondaryAddress = "../folder2/";
+	const std::string& result = "http://www.site1.com:1234/folder2";
+	const std::string& assume = combiner(primaryAddress, secondaryAddress);
+	EXPECT_EQ(assume, result);
+}
+
+TEST(AddressCombinerTest, EnteredSecondaryWithDoubleDotSlashOnly_ExpectedNoChangingAdrees) {
+	std::string primaryAddress = "http://www.site1.com:1234/";
+	std::string secondaryAddress = "../";
+	const std::string& result = "http://www.site1.com:1234";
+	const std::string& assume = combiner(primaryAddress, secondaryAddress);
+	EXPECT_EQ(assume, result);
+}
+
+TEST(AddressCombinerTest, EnteredSecondaryWithDotSlashOnly_ExpectedNoChangingAdrees) {
+	std::string primaryAddress = "http://www.site1.com:1234/";
+	std::string secondaryAddress = "./";
+	const std::string& result = "http://www.site1.com:1234";
+	const std::string& assume = combiner(primaryAddress, secondaryAddress);
+	EXPECT_EQ(assume, result);
+}
 
 TEST(AddressCombinerTest, EnteredPrimaryAndSecondaryWithExtraSlashesAfter_ExpectedCombineRighBothAddress) {
 	std::string primaryAddress = "http://www.site1.com:1234/folder1//////////////";
@@ -103,10 +111,24 @@ TEST(AddressCombinerTest, EnteredPrimaryAndSecondaryWithExtraSlashesAfter_Expect
 }
 
 
-TEST(AddressCombinerTest, AddressCombiner_TrimTestForBothAddresses_ExpectedCombineRighBothAddress) {
-	std::string primaryAddress = "        http://www.site1.com:1234/folder1//////////////               ";
-	std::string secondaryAddress = "                 folder2////////////         ";
-	const std::string& result = "http://www.site1.com:1234/folder1/folder2";
+
+
+
+////to fix !!!
+TEST(AddressCombinerTest, EnteredPrimaryNoSlashSecondaryWithSlash_ExpectedCombineRighBothAddress) {
+	std::string primaryAddress = "http://www.site1.com:1234/folder1";
+	std::string secondaryAddress = "/folder2/";			//WRONG !!!
+	const std::string& result = "http://www.site1.com:1234/folder2";
 	const std::string& assume = combiner(primaryAddress, secondaryAddress);
 	EXPECT_EQ(assume, result);
 }
+
+//to fix !!!
+TEST(AddressCombinerTest, EnteredPrimaryNoSlashSecondaryWithDoubleSlash_ExpectedCombineRighBothAddress) {
+	std::string primaryAddress = "http://www.site1.com:1234/folder1";
+	std::string secondaryAddress = "//site2.com/folder2/";
+	const std::string& result = "http://site2.com/folder2";
+	const std::string& assume = combiner(primaryAddress, secondaryAddress);
+	EXPECT_EQ(assume, result);
+}
+
