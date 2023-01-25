d73545a49b3be1106af20f55bde6ae6d473f40f5
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2023-01-25 14:27:47 +0200

    update

--- CrawlerLib/ICrawlerRun.h
+++ CrawlerLib/ICrawlerRun.h
@@ -1,13 +1,12 @@
 #pragma once
 
-
 class ICrawlerRun	//set to interface
 {
 public:
 	virtual void print() const = 0;
 	virtual void write_to_file(const string& file_address_name) const = 0;
 	virtual string to_string() const = 0;
-protected:
+protected:	//internal use and testing methods
 	virtual void init(const std::string begin_address, size_t crawler_levels) = 0;
 	virtual string get_html(const string& uri) = 0;
 	virtual void search_for_links(GumboNode* node, const string& uri, const size_t& level) = 0;
