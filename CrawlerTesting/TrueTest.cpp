012c8b4b260e5bbbe443514b2b4f3c08e4900f5f
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2023-01-26 03:47:03 +0200

    update

--- CrawlerTesting/TrueTest.cpp
+++ CrawlerTesting/TrueTest.cpp
@@ -2,7 +2,7 @@
 
 //#include "AddressCombiner.h"
 
-TEST(TrueTest, NoNameMethodTest_EnteredTrueScenario_ExpectedGetTrue) {
+TEST(TrueTest, DISABLED_NoNameMethodTest_EnteredTrueScenario_ExpectedGetTrue) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
